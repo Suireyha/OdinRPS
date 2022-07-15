ac7711f9ac30b4fce5ba05c409468d7c6422c5b5
Suireyha <Marvink2296@gmail.com>
2022-07-16 06:09:17 +1000

    Added author notes

--- script.js
+++ script.js
@@ -92,3 +92,11 @@ function playRPS(select){
 btnRock.cont.addEventListener('click', ()=>{playRPS(move[0]);});
 btnPaper.cont.addEventListener('click', ()=>{playRPS(move[1])});
 btnScissors.cont.addEventListener('click', ()=>{playRPS(move[2])});
+
+/* I didn't actually read the asignment, so I know that this is probably a bit unorthodox comparatively,
+ * but I did go back and read that there were meant to be rounds involved.
+ * This would be easy enough to add, just make a matchCounter variable, as well as two winCounters for the AI and the Player.
+ * After (matchCounter == matchAmount) compare winCountAI and winCountPlr. If AI > Plr, the AI won, else the player won. 
+ * 
+ * Just can't really be assed to add it in because the code is spaghetti as is, and I have to leave room for some improvement in the later chapter. ;)
+ */
