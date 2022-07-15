b412fea2539d0e4c084beee2e8274f1db639f1a6
Suireyha <Marvink2296@gmail.com>
2022-07-15 11:58:46 +1000

    Added buttons with click functionality

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("This thing on?");
+var btnRock = document.querySelector('.rock');
+
+var btnPaper = document.querySelector('.paper');
+
+var btnScissors = document.querySelector('.scissors');
+
+btnRock.addEventListener('click', ()=>{console.log("It's about drive")});
+
+btnPaper.addEventListener('click', ()=>{console.log("I love you!")});
+
+btnScissors.addEventListener('click', ()=>{console.log("Anime watcher")});
